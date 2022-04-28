9d88a64315135903f27783203dceabc9233d5954
VeniMitev <97586359+VeniMitev@users.noreply.github.com>
2022-04-28 01:01:10 -0700

    Optimizing files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import logo from '../../logo.svg';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
